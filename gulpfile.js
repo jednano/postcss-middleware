e1aa03a8b288768e857b98af5ee148e85c55be3b
Jed Mao <jedmao@outlook.com>
2015-07-07 18:15:45 -0500

    Fix CodeClimate

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ gulp.task('lint', ['eslint', 'tslint']);
 gulp.task('eslint', loadTask('eslint'));
 gulp.task('tslint', loadTask('tslint'));
 gulp.task('test', ['scripts'], loadTask('test'));
+gulp.task('test:ci', loadTask('test'));
 gulp.task('scripts', ['clean', 'tslint'], loadTask('scripts'));
 gulp.task('clean', loadTask('clean'));
 gulp.task('watch', ['default'], loadTask('watch'));
